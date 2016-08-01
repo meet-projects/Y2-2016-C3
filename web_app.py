b847589b71ffc73dc1d41f36fa815f3378058731
Ramesh Sridharan <rameshvs@csail.mit.edu>
2016-08-01 08:05:13 +0300

    improvements and updates

--- web_app.py
+++ web_app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # SQLAlchemy stuff
-#from database_setup import Base Person <--- Import your tables here!!
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-#engine = create_engine('sqlite:///crudlab.db')
-#Base.metadata.bind = engine
-#DBSession = sessionmaker(bind=engine)
-#session = DBSession()
+### Add your tables here!
+# For example:
+# from database_setup import Base, Potato, Monkey
+from database_setup import Base
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('sqlite:///project.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 #YOUR WEB APP CODE GOES HERE
